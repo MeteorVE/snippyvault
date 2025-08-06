348ffbadbb1ca9ba2339528abb48a227a88549a0
meteorxu <meteorxu@m800.com>
2025-08-06 18:31:46 +0800

    edit

--- js/config.prod.js
+++ js/config.prod.js
@@ -1,4 +1,5 @@
 window.SNIPPYVAULT_CONFIG = {
-    API_BASE_URL: "https://wish-list-local.m1t2.link/backend/snippyvault/v1"
+    //API_BASE_URL: "https://wish-list-local.m1t2.link/backend/snippyvault/v1"
+    API_BASE_URL: "http://127.0.0.1:5001"
 };
 
